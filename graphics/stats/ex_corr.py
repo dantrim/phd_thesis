28a0aacec61db1d10a7c2fe9e49c70e093691107
Daniel Joseph Antrim <daniel.joseph.antrim@cern.ch>
2019-09-26 23:42:20 +0200

    prof examples and discussion

---
+++ graphics/stats/ex_corr.py
@@ -0,0 +1,114 @@
+#!/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sys, os
+
+def example_1() :
+
+    fig, ax = plt.subplots(1,1)
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+
+    data_grid = {
+        (0,0) : 0,
+        (0,1) : 0,
+        (0,2) : 1,
+        (1,0) : -0.5,
+        (1,1) : 1,
+        (1,2) : 0,
+        (2,0) : 1,
+        (2,1) : -0.5,
+        (2,2) : 0
+    }
+    bw = 1
+    bins = np.arange(0, 4, bw)
+    ax.tick_params(which = "both", direction = "out")
+    ax.set_xticks(bins[:-1]+0.5*bw)
+    ax.set_yticks(bins[:-1]+0.5*bw)
+    ax.set_xticklabels(["Norm. Bkg 1", r"Norm. Bkg 2$_1$", r"Norm. Bkg 2$_2$"])
+    ax.set_yticklabels([r"Norm. Bkg 2$_2$", r"Norm. Bkg 2$_1$", "Norm. Bkg. 1"],
+        rotation = 50)
+    #sys.exit()
+
+    x_vals = []
+    y_vals = []
+    w_vals = []
+    for dp, w in data_grid.items() :
+        x, y = dp
+        x_vals.append(x)
+        y_vals.append(y)
+        w_vals.append(w)
+
+
+    h, x, y, im = ax.hist2d(x_vals, y_vals, bins = bins, weights = w_vals)
+    cbar = fig.colorbar(im)
+    cbar.set_label("NP Correlation", horizontalalignment = "right", y = 1)
+
+    for dp, w in data_grid.items() :
+        ax.text(dp[0]+0.4 * bw, dp[1]+0.45 * bw, r"$\mathbf{%.2f}$" % float(w), color = "w")
+
+    fig.show()
+    fig.savefig("np_corr_ex_1.pdf", bbox_inches = "tight")
+    x = input()
+
+def example_2() :
+
+    fig, ax = plt.subplots(1,1)
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+
+    data_grid = {
+        (0,0) : -0.97,
+        (0,1) : 0,
+        (0,2) : 0,
+        (0,3) : 1,
+        (1,0) : 0,
+        (1,1) : -0.5,
+        (1,2) : 1,
+        (1,3) : 0,
+        (2,0) : 0,
+        (2,1) : 1,
+        (2,2) : -0.5,
+        (2,3) : 0,
+        (3,0) : 1,
+        (3,1) : 0,
+        (3,2) : 0,
+        (3,3) : -0.97
+        
+    }
+    bw = 1
+    bins = np.arange(0, 5, bw)
+    ax.tick_params(which = "both", direction = "out")
+    ax.set_xticks(bins[:-1]+0.5*bw)
+    ax.set_yticks(bins[:-1]+0.5*bw)
+    ax.set_xticklabels(["Norm. Bkg 1", r"Norm. Bkg 2$_1$", r"Norm. Bkg 2$_2$", r"$\mu$"])
+    ax.set_yticklabels([r"$\mu$", r"Norm. Bkg 2$_2$", r"Norm. Bkg 2$_1$", "Norm. Bkg. 1"],
+        rotation = 50)
+    #sys.exit()
+
+    x_vals = []
+    y_vals = []
+    w_vals = []
+    for dp, w in data_grid.items() :
+        x, y = dp
+        x_vals.append(x)
+        y_vals.append(y)
+        w_vals.append(w)
+
+
+    h, x, y, im = ax.hist2d(x_vals, y_vals, bins = bins, weights = w_vals)
+    cbar = fig.colorbar(im)
+    cbar.set_label("NP Correlation", horizontalalignment = "right", y = 1)
+
+    for dp, w in data_grid.items() :
+        ax.text(dp[0]+0.4 * bw, dp[1]+0.45 * bw, r"$\mathbf{%.2f}$" % float(w), color = "w")
+
+    fig.show()
+    fig.savefig("np_corr_ex_2.pdf", bbox_inches = "tight")
+    x = input()
+
+if __name__ == "__main__" :
+
+    #example_1()
+    example_2()
---
+++ graphics/stats/javier_one.py
@@ -0,0 +1,150 @@
+#!/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os, sys
+import argparse
+
+def do_plot(post = False) :
+
+    data_vals = [100, 100]
+    bkg_vals = [80, 100]
+    bkg_uncerts = [0.1, 0.14]
+    if post :
+        bkg_vals = [88, 100]
+        bkg_uncerts = [0.07, 0.1]
+
+    fig, ax = plt.subplots(1,1)
+    ax.set_xticks([0.5, 1.5])
+    ax.set_xticklabels(["Region 1", "Region 2"])
+    ax.set_ylabel("Yield", horizontalalignment = "right", y = 1)
+    ax.set_xlabel("")
+    ax.set_xlim([0,2])
+    ax.set_ylim([0,180])
+    ax.tick_params(which = "both", direction = "in",
+        top = True, right = True
+    )
+
+    # draw mc
+    ax.bar([0.5], bkg_vals[0], color = "#bb2a33",
+        width = 1,
+		label = "Bkg 1"
+    )
+    ax.bar([1.5], bkg_vals[1], color = "#286fb0",
+        width = 1,
+		label = "Bkg 2"
+    )
+
+    # draw mc error bars
+    err_lo = bkg_vals[0] - bkg_uncerts[0] * bkg_vals[0]
+    err_hi = bkg_vals[0] + bkg_uncerts[0] * bkg_vals[0]
+    e_lo = np.array([err_lo, err_lo])
+    e_up = np.array([err_hi, err_hi])
+    x =  np.array([0,1])
+    ax.fill_between(x, e_lo, e_up, hatch = "///",
+        facecolor = "none", edgecolor = "k", linewidth = 0, zorder = 1e9)
+
+    err_lo = bkg_vals[1] - bkg_uncerts[1] * bkg_vals[1]
+    err_hi = bkg_vals[1] + bkg_uncerts[1] * bkg_vals[1]
+    e_lo = np.array([err_lo, err_lo])
+    e_up = np.array([err_hi, err_hi])
+    x =  np.array([1,2])
+    ax.fill_between(x, e_lo, e_up, hatch = "///",
+        facecolor = "none", edgecolor = "k", linewidth = 0, zorder = 1e9)
+
+    # draw data
+    data_err = [10, 10]
+    ax.plot([0.5, 1.5], data_vals, "ko",
+		label = "Data",
+		markersize = 8
+	)
+    ax.errorbar([0.5, 1.5], data_vals, yerr = data_err, xerr = 0,
+        linestyle = "none",
+        marker = "o",
+        color = "k",
+        markersize = 0,
+		linewidth = 2
+    )
+
+	# legend
+    ax.legend(loc = "upper right", frameon = False, prop = {"size" : 15})
+
+    # labels
+    label = "Pre-fit"
+    if post :
+        label = "Post-fit"
+    ax.text(0.1, 160, label, size = 20, weight = "bold")
+    ax.text(0.1, 150, "Bkg 1 Pre-fit Uncertainty: 10%", size = 10)
+
+    fig.show()
+    outname = "profile_ex_1_NP10_"
+    if post :
+    	outname += "post"
+    else :
+    	outname += "pre"
+    outname += ".pdf"
+    fig.savefig(outname, bbox_inches = "tight")
+    x = input()
+
+def do_pull() :
+
+    bkg0 = [0.469, 0.241]
+    bkg0_10p = [1.041, 0.74]
+    bkg1_0 = [0, 0.814]
+    bkg1_1 = [0, 0.814]
+
+    fig, ax = plt.subplots(1,1)
+    ax.tick_params(which = "both", direction = "in",
+        top = True, right = True
+    )
+
+    x_lims = [0, 3]
+    y_lims = [-2, 2]
+    ax.set_xlim(x_lims)
+    ax.set_ylim(y_lims)
+    ax.set_xlabel("")
+    ax.set_ylabel(r"Post Fit $\theta$")
+    ax.set_xticks([0.5, 1.5, 2.5])
+    ax.set_xticklabels(["Norm. Bkg 1", r"Norm. Bkg 2$_1$", r"Norm. Bkg 2$_2$"])
+
+    x_vals = [0.4, 1.5, 2.5]
+    y_vals = [bkg0[0], bkg1_0[0], bkg1_1[0]]
+    y_errs = [bkg0[1], bkg1_0[1], bkg1_1[1]]
+
+    ax.errorbar(x_vals, y_vals, yerr = y_errs, xerr = 0,
+        linestyle = "none",
+        color = "k",
+        marker = "o",
+        linewidth = 1,
+        markersize = 6,
+        capsize = 2
+    )
+    x_vals = [0.6]
+    y_vals = [bkg0_10p[0]]
+    y_errs = [bkg0_10p[1]]
+    ax.errorbar(x_vals, y_vals, yerr = y_errs, xerr = 0,
+        linestyle = "none",
+        color = "m",
+        marker = "o",
+        linewidth = 1,
+        markersize = 6,
+        capsize = 2
+    )
+
+    # lines
+    ax.plot(x_lims, [0,0], "r--")
+    ax.plot(x_lims, [1,1], "b-")
+    ax.plot(x_lims, [-1,-1], "b-")
+
+    fig.show()
+    fig.savefig("profile_ex_1_pulls.pdf", bbox_inches = "tight")
+    x = input()
+
+def main() :
+
+    #do_plot(post = False)
+    #do_plot(post = True)
+    do_pull()
+
+if __name__ == "__main__" :
+    main()
